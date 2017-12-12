e2cb966751228d41da29f4ce8e6ad6a61cb89434
gastonfeng <gastonfeng@gmail.com>
2017-12-12 15:24:01 +0800

    reply

--- controllers/handlers/menu_click.py
+++ controllers/handlers/menu_click.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import logging
 
 from odoo.http import request
 from ..routes import robot
@@ -7,7 +8,9 @@ from ..routes import robot
 @robot.click
 def onclick(message, session):
     _name, action_id = message.key.split(',')
+    logging.info('onclick : _name=%s ,action_id =%s' % (_name, action_id))
     action_id = int(action_id)
     if _name:
         action = request.env()[_name].sudo().browse(action_id)
+        logging.info(str(action))
         return action.get_wx_reply()
