e58e33b7d1cd63903e9a2288f2adae83ced8e670
gastonfeng <gastonfeng@gmail.com>
2017-12-15 14:55:24 +0800

    reply

--- controllers/handlers/menu_click.py
+++ controllers/handlers/menu_click.py
@@ -8,7 +8,7 @@ from ..routes import robot
 @robot.click
 def onclick(message, session):
     _name, action_id = message.key.split(',')
-    logging.info('onclick : _name=%s ,action_id =%s' % (_name, action_id))
+    logging.info('onclick : message=%s,key=%s,_name=%s ,action_id =%s' % (message, message.key, _name, action_id))
     action_id = int(action_id)
     if _name:
         action = request.env()[_name].sudo().browse(action_id)
